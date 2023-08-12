4a064bed93822317a4d0b33926719fcc86daa898
cagilbert <cag21003@utdallas.edu>
2023-08-11 20:50:11 -0500

    Final

---
+++ Assets/DialogueSystem/Scripts/GameComponents/DialogueManagerComponent.cs
@@ -0,0 +1,94 @@
+﻿namespace DialogueManager.GameComponents
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using DialogueManager.Controllers;
+    using DialogueManager.Models;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// This class manages the text in the dialogues, the transition between sentences, animations, and such
+    /// </summary>
+    public class DialogueManagerComponent : MonoBehaviour
+    {
+        /// <summary> Model of the Dialogue Manager </summary>
+        public DialogueManager Model;
+
+        /// <summary> Controller of the Dialogue Manager </summary>
+        private DialogueManagerController controller;
+
+        /// <summary>
+        /// Is excecuted when the object is instantiated
+        /// </summary>
+        private void Awake()
+        {
+            GameObject gameConversations = Instantiate( this.Model.GameConversationsPrefab );
+            gameConversations.name = "GameConversations";
+
+            /*
+            GameObject canvasObject = new GameObject( "DialogueCanvas", typeof(RectTransform) );
+            Canvas canvas = canvasObject.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.WorldSpace;
+            */
+            Transform canvasObject = GameObject.Find( "DialogueCanvas" ).GetComponent<Transform>();
+            GameObject dialogueBox = Instantiate( this.Model.CanvasObjectsPrefab );
+            dialogueBox.transform.position = new Vector3( -250, 0, 0 );
+            dialogueBox.name = "DialogueBox";
+            dialogueBox.transform.SetParent( canvasObject.transform );
+            dialogueBox.GetComponent<RectTransform>().localPosition = new Vector3( 0, -500, 0 );
+
+
+            this.Model.DialogueStartPoint = GameObject.Find( "/DialogueCanvas/DialogueBox/DialogueStartPoint" ).GetComponent<Transform>();
+            this.Model.ImageText = GameObject.Find( "/DialogueCanvas/DialogueBox/CharacterImage" ).GetComponent<Image>();
+            this.Model.Animator = GameObject.Find( "/DialogueCanvas/DialogueBox" ).GetComponent<Animator>();
+            this.Model.Source = this.GetComponent<AudioSource>();
+
+            this.controller = new DialogueManagerController( this.Model );
+        }
+
+        /// <summary>
+        /// Checks if there is something in the model to display and if there was an input
+        /// </summary>
+        private void Update()
+        {
+            if ( this.Model.DialogueToShow != null )
+            {
+                this.StartDialogue();
+            }
+
+            if ( Input.GetKeyDown( this.Model.NextKey ) && this.Model.Finished && this.Model.DoubleTap )
+            {
+                this.DisplayNextSentence();
+                this.Model.Finished = false;
+            }
+
+            if ( Input.GetKeyDown( this.Model.NextKey ) && this.Model.DoubleTap == false )
+            {
+                this.Model.Finished = true;
+                this.DisplayNextSentence();
+            }
+        }
+
+        /// <summary>
+        /// Start new dialogue, and reset all data from previous dialogues
+        /// </summary>
+        private void StartDialogue()
+        {
+            this.controller.StartDialogue();
+            this.DisplayNextSentence();
+        }
+
+        /// <summary>
+        /// Display next sentence in dialogue
+        /// </summary>
+        private void DisplayNextSentence()
+        {
+            this.StopAllCoroutines();
+            if ( this.controller.DisplayNextSentence() )
+            {
+                this.StartCoroutine( this.controller.TypeSentence() );
+            }
+        }
+    }
+}
---
+++ Assets/DialogueSystem/Scripts/Models/DialogueManager.cs
@@ -0,0 +1,58 @@
+﻿namespace DialogueManager.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// Model of the main Dialogue Manager
+    /// </summary>
+    [Serializable]
+    public class DialogueManager
+    {
+        /// <summary> Prefabs of the Dialogue Box, text and image. </summary>
+        public GameObject CanvasObjectsPrefab;
+
+        /// <summary> Prefab of the GameConversations. </summary>
+        public GameObject GameConversationsPrefab;
+
+        /// <summary> Time between each letter. </summary>
+        public float WaitTime = .01f;
+
+        /// <summary> Volume of the Voice of the characters. </summary>
+        public float VoiceVolume = 1f;
+
+        /// <summary> Is double tap. </summary>
+        public bool DoubleTap = true;
+
+        /// <summary> Key which must be pressed to continue to the next Sentence. </summary>
+        public string NextKey = "z";
+
+        /// <summary> Font </summary>
+        public Font Font;
+
+        /// <summary> FontMaterial </summary>
+        public Material Material;
+
+        /// <summary> Gets or sets the Text that is being displayed on the Scene. </summary>
+        public Transform DialogueStartPoint { get; set; }
+
+        /// <summary> Gets or sets the Image that is being displayed on the Scene. </summary>
+        public Image ImageText { get; set; }
+
+        /// <summary> Gets or sets the Animation that causes the Dialogue box to go up or down. </summary>
+        public Animator Animator { get; set; }
+
+        /// <summary> Gets or sets the Audio that the current dialogue is showing. </summary>
+        public AudioSource Source { get; set; }
+
+        /// <summary> Gets or sets a value indicating whether the Dialogue has finished or not. </summary>
+        public bool Finished { get; set; }
+
+        /// <summary> Gets or sets the <see cref="Dialogue"/> that will be displayed. </summary>
+        public Dialogue DialogueToShow { get; set; }
+    }
+}
